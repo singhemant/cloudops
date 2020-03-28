6a3f6e5570e3bf7c727b28aa9b0107648d2d8b8b
Singh, Hemant <hesingh@Collegeboard.org>
2020-03-28 11:42:57 -0400

    Updates

---
+++ precache-manifest.b4e8bd5da9d7ae5e73315638a511ddfe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc43de5b31e0af1b32299ff91e3ee73",
+    "url": "/cloudops/index.html"
+  },
+  {
+    "revision": "cdc51fbf89120964d269",
+    "url": "/cloudops/static/css/main.2a7d9bbf.chunk.css"
+  },
+  {
+    "revision": "ad48dfdedf3467ab88d3",
+    "url": "/cloudops/static/js/2.415c7278.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cloudops/static/js/2.415c7278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdc51fbf89120964d269",
+    "url": "/cloudops/static/js/main.231bef85.chunk.js"
+  },
+  {
+    "revision": "1fb83f590a2a2e29c31e2c9b74e01f43",
+    "url": "/cloudops/static/js/main.231bef85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e78a9578de9d264849f",
+    "url": "/cloudops/static/js/runtime-main.2cf9411e.js"
+  },
+  {
+    "revision": "0f3fa4b3c7c0cc233fb8e96460a68358",
+    "url": "/cloudops/static/media/header-bg.0f3fa4b3.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cloudops/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/cloudops/static/media/map-image.f40ed5b1.png"
+  },
+  {
+    "revision": "1b030af06015c79befdd9674756193c2",
+    "url": "/cloudops/static/media/portfolio3.1b030af0.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.13651844cde6991732f36bfb98b8fe9d.js"
+  "/cloudops/precache-manifest.b4e8bd5da9d7ae5e73315638a511ddfe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cloudops/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
